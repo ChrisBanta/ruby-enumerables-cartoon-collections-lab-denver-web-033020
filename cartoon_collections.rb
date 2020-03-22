04304066e2dee9f3b6a8976c8051aac5a6dcc846
Chris Banta <chrisbanta1@gmail.com>
2020-03-22 16:29:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 
 def find_the_cheese(snacks)
-  cheese = ["cheddar"|| "gouda"|| "camembert"]
+  cheese == ["cheddar"|| "gouda"|| "camembert"]
   snacks.find do |cheese|
   cheese.include?(cheese)
  end
