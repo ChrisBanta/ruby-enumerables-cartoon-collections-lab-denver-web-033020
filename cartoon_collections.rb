a849f04848aa79cdbaec6368a481cbec3954619e
Chris Banta <chrisbanta1@gmail.com>
2020-03-22 15:55:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,6 +7,6 @@ def roll_call_dwarves(array)
  end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map! {|planeteer_calls| planeteer_calls.capitalize}
-  planeteer_calls.collect {|planeteer_calls| puts planeteer_calls +'!'}
+  planeteer_calls.map! {|call|call.capitalize + '!'}
+  
 end
