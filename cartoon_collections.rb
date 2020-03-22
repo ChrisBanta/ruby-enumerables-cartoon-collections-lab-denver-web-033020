c0da7be0567c9746303600c4655913babcb41359
Chris Banta <chrisbanta1@gmail.com>
2020-03-22 16:02:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,3 +13,9 @@ def summon_captain_planet(planeteer_calls)
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any?{ |call| call.length > 4}
 end
+
+
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
